67ce158dd75437e2771a8688e1d2107ae8369825
Dev Siracusa <dev@siracusa.app>
2026-06-23 09:15:46 -0400

    fix: remove CSS imports

--- src/components/ui/VisorPlano.tsx
+++ src/components/ui/VisorPlano.tsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import "react-pdf/dist/esm/Page/TextLayer.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
