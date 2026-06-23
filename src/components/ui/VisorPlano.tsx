802b21ec595890cba554aaf8592b228d059db42a
Dev Siracusa <dev@siracusa.app>
2026-06-23 09:14:46 -0400

    fix: configurar worker de pdfjs + CSS

--- src/components/ui/VisorPlano.tsx
+++ src/components/ui/VisorPlano.tsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from "react";
-import { Document, Page } from "react-pdf";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 interface Props {
   url: string;
